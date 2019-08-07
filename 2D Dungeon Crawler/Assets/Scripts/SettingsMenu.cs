35fe698f67e17625b4147ccb9a8a5ffd63658d03
CallumDaws <callumdaws@hotmail.co.uk>
2019-08-07 11:07:49 +0100

    Updated Assets and additional levels

---
+++ 2D Dungeon Crawler/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+	public AudioMixer audioMixer;
+
+	public Dropdown resolutionDropdown;
+
+	public GameObject mainMenu;
+
+	Resolution[] resolutions;
+
+	public bool hide;
+
+
+	void Start()
+	{
+
+		resolutions = Screen.resolutions;
+		Debug.Log (resolutions);
+
+		resolutionDropdown.ClearOptions();
+
+		List<string> options = new List<string>();
+
+		int currentResolutionIndex = 0;
+
+		for(int i =0; i<resolutions.Length; i++){
+			string option = resolutions[i].width + " x " + resolutions[i].height + " (" + resolutions[i].refreshRate + "hz)";
+			options.Add(option);
+			if (resolutions[i].width == Screen.width && 
+				resolutions[i].height == Screen.height && resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+			{
+				currentResolutionIndex = i;
+			}
+		}
+
+		resolutionDropdown.AddOptions(options);
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
+	}
+
+	public void backButtonConfig(){
+		if (hide == true) {
+			mainMenu.SetActive (true);
+		} else {
+			mainMenu.SetActive (false);
+		}
+	}
+
+
+	public void setVolume(float volume){
+		audioMixer.SetFloat ("volume", volume);
+}
+
+	public void setQuality(int qualityValue){
+		QualitySettings.SetQualityLevel (qualityValue);
+}
+		
+	public void setFullscreen(bool isFullscreen){
+		Screen.fullScreen = isFullscreen;
+	}
+
+	public void setResolution(int resolutionValue){
+		Resolution resolution = resolutions[resolutionValue];
+		Screen.SetResolution (resolution.width, resolution.height, Screen.fullScreen);
+	}
+		
+}
---
+++ 2D Dungeon Crawler/Assets/Scripts/StatItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatItem : Item
+{
+    public Player player;
+    public KnockBack KnockBack;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.Equals(player)) ;
+    }
+}
